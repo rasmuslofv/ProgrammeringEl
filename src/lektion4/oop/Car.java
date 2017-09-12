55d870a301dbf7b68011149f2513ff9a77711c97
rasmus90lofv <rasmus.lofvenhamn@sundbyberg.it-gymnasiet.se>
2017-09-12 11:11:43 +0200

    lektion4

---
+++ src/lektion4/oop/Car.java
@@ -0,0 +1,21 @@
+package lektion4.oop;
+
+public class Car {
+    private int numberOfDoors = 5;
+    private int numberOfWheels = 4;
+    private String color = "blue";
+    private int antaletRattar = 1;
+    private String carBrand = "Volvo";
+    private int speed = 0;
+    
+    
+    
+    public void setSpeed(int newSpeed) {
+           speed = newSpeed;
+    
+    }
+    public int getSpeed() {
+		return speed;
+    	
+    }
+}
---
+++ src/lektion4/oop/CarGame.java
@@ -0,0 +1,16 @@
+package lektion4.oop;
+
+public class CarGame {
+
+	public static void main(String[] args) {
+	    Car kimjongBil = new Car();	
+        
+	   System.out.println(kimjongBil.getSpeed());
+	    
+	   kimjongBil.setSpeed(2000); 
+	   System.out.println(kimjongBil.getSpeed());
+	
+	
+	}
+
+}
