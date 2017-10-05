06a7aee1f31a5d3f74c76e74412eb416935428b2
rasmus90lofv <rasmus.lofvenhamn@sundbyberg.it-gymnasiet.se>
2017-10-05 10:15:28 +0200

    haha

---
+++ src/Rectangle/Employee.java
@@ -0,0 +1,39 @@
+package Rectangle;
+
+public class Employee {
+    private int id;{
+    private String lastName
+    private String firstName
+    }
+    private String firstName() {
+		return null;
+    	
+    	
+    }
+    private String lastName() {
+		return null;
+    
+    	
+    }
+    
+    
+    
+    public int getID() {
+		return id;
+    }
+    public String getfirstName() {
+		return firstName();
+    	
+    }
+    public String getlastName() {
+		return firstName();
+ }
+    public String getName() {
+		return null;	
+    }
+    public int getSalary() {
+		return id;
+    	
+    }
+    
+}  
---
+++ src/Rectangle/Rectangle.java
@@ -0,0 +1,40 @@
+package Rectangle;
+
+public class Rectangle {
+	private float length = 1.0f;
+	private float width = 1.0f;
+
+	public Rectangle() {
+
+	}
+	/**
+	 * Konstruktor 
+	 * @param length 
+	 * @param width
+	 */
+	public Rectangle(float length, float width) {
+		this.length = length;
+		this.width = width;
+	}
+	/**
+	 * Returnerar längden på rectangeln
+	 * @return
+	 */
+	public float getLength() {
+		return length;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public double getArea() {
+		return length * width;
+
+	}
+
+	public double getPerimeter() {
+		return 2*(width + length);
+
+	}
+}
